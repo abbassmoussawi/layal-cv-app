dad2decd81edbf89c492179c30a76e73945dd69b
abbassmoussawi <abbass.y.moussawi@gmail.com>
2023-09-26 22:21:31 +0300

    editing the code of 'edit-post'page

--- public/staff/authors/edit-post.php
+++ public/staff/authors/edit-post.php
@@ -4,35 +4,6 @@ session_start();
 require_once('../../../private/initialize.php');
 $id = $_GET['id'];
 $post = Post::find_by_id($id);
-// if (isset($id)) {
-    // foreach ($_POST as $file => $description) {
-    //     echo "{$file} : $description" . '<br>';
-    // }
-  
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        
-        $info['title']= $_POST['title'];
-        $info['content'] = $_POST['content'];
-        $info['author'] = $_POST['author'];
-        $info['time']= $_POST['time'];
-
-        $filename = $info['image_url']= $_FILES["imagename"]["name"];
-        $tempname = $_FILES["imagename"]["tmp_name"];
-        $folder = '../../../images-server/'. $filename;
-
-        if (move_uploaded_file($tempname, $folder)) {
-            echo "<h3>  Image Updated Successfully!</h3>";
-        } else {
-            echo "<h3>  Failed To Update Image!</h3>";
-        }
-        
-        $post= new Post($info);
-        $result = $post ->update($id);
-        if($result){
-            redirect_to(url_for('staff/main-author/main-author-posts.php'));
-        }
-    }
-
 ?>
 
 <div class="shared-frame">
@@ -43,11 +14,10 @@ $post = Post::find_by_id($id);
 
         <label for="post-image">Post image</label>
         <div>
-            <img src="<?php echo"../../../../layal-webapp/images-server/". $post->image_url; ?>" class="post-image">
+            <img src="../../../public/images-server/<?= $post->image_url; ?>" name="image_name" class="post-image">
         </div>
-        
-        <input type="file" id="post-image" name="imagename" value="<?php echo"../../../../layal-webapp/images-server/". $post->image_url; ?>">
 
+        <input type="file" id="post-image" name="image_name">
 
         <label for="post-title">Post's title</label>
         <input type="text" id="title" name="title" value="<?= $post->title; ?>">
@@ -57,20 +27,48 @@ $post = Post::find_by_id($id);
         <textarea id="content" name="content" rows="5" cols="33"><?= $post->content; ?></textarea>
 
 
-        <label for="author">Post's Author:<strong> <?=$post->author;?></strong></label>
+        <label for="author">Post's Author:<strong>
+                <?= $post->author; ?>
+            </strong></label>
         <select name="author" id="author">
-            <option value="<?=$post->author;?>"><?=$post->author;?></option>
-            <option value="Layal Aabed">Layal Aabed</option>
-            <option value="Abbass El Moussawi">Abbass El Moussawi</option>
+
         </select>
-
-        <label for="posting-date">Posting Time</label>
-        <?=$post->time;?>
-
-        <input type="datetime-local" id="posting-date" name="time" value="<?=$post->time;?>" />
 
         <button type="submit" name="post" class="button">UPDATE</input>
     </form>
 </div>
 
-<?php  include SHARED_PATH.'/footer.php'; ?>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $info['title'] = $_POST['title'];
+    $info['content'] = $_POST['content'];
+    $info['time'] = $post->time;
+
+    echo '<script type="text/javascript">';
+    if (document . getElementById('post-image') . files . length == 1) {
+        ;
+        echo '</script>';
+
+        $filename = $info['image_url'] = $_FILES["imagename"]["name"];
+        $tempname = $_FILES["imagename"]["tmp_name"];
+        $folder = '../../images-server/' . $filename;
+
+        echo '<script type="text/javascript">';
+
+    }
+    ;
+    echo '</script>';
+
+    $info['image_url'] = $post->image_url;
+    $info['author'] = $post->author;
+
+    $post = new Post($info);
+    $result = $post->update($id);
+    // dd($result);
+    if ($result) {
+        redirect_to(url_for('staff/main-author/main-author-posts.php'));
+    }
+}
+?>
+<?php include SHARED_PATH . '/footer.php'; ?>
